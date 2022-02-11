45830648e91750a386ef0079597f3f64742ed70a
Stephen Strange <80538114+Strange-bs@users.noreply.github.com>
2022-02-11 08:30:15 +0500

    Popups added

--- js/strange.js
+++ js/strange.js
@@ -241,15 +241,16 @@ function setVariable() {
     "gi"
   );
   var attributes = {};
-  while ((match = regex.exec(string))) {
+  while (match = regex.exec(string)) {
     attributes[match[1]] += match[3] + ",";
   }
+    console.log(attributes)
   if (attributes.id) {
     var id = attributes.id;
     id = id.replace("undefined", "");
     id = id.split(",");
-    id = id.filter((m) => m);
-    for (let s = 0; s < id.length; s++) {
+    id = id.filter(function(m) {return m;});
+    for (var s = 0; s < id.length; s++) {
       window[`$${id[s]}`] = document.querySelector(`#${id[s]}`);
       window[`$${id[s]}s`] = document.querySelectorAll(`#${id[s]}`);
     }
@@ -258,8 +259,8 @@ function setVariable() {
     var className = attributes.class;
     className = className.replace("undefined", "");
     className = className.split(",");
-    className = className.filter((f) => f);
-    for (let c = 0; c < className.length; c++) {
+    className = className.filter(function(f){return f;});
+    for (var c = 0; c < className.length; c++) {
       window[`$_${className[c]}`] = document.querySelector(`.${className[c]}`);
       window[`$_${className[c]}s`] = document.querySelectorAll(
         `.${className[c]}`
@@ -328,37 +329,12 @@ var is = {
   },
 };
 
-function floor(a) {
-  if (a !== undefined) {
-    return Math.floor(a);
-  } else {
-    console.error(err("floor", 1));
-  }
-}
+let floor = Math.floor
 
-function round(i) {
-  if (i !== undefined) {
-    return Math.round(i);
-  } else {
-    console.error(err("round", 1));
-  }
-}
+let round = Math.round;
 
-function ceil(x) {
-  if (x !== undefined) {
-    return Math.ceil(x);
-  } else {
-    console.error(err("ceil", 1));
-  }
-}
+let ceil = Math.ceil;
 
-function log(a) {
-  if (a !== undefined) {
-    console.log(a);
-  } else {
-    console.error(err("log", 1));
-  }
-}
 
 function pow(a, b) {
   if (a !== undefined && b !== undefined) {
@@ -370,31 +346,31 @@ function pow(a, b) {
   }
 }
 
-function random(max, min) {
+function random(min, max) {
   if (max == undefined && min == undefined) {
-    console.error(err("random", 1));
-  } else if (min == undefined && max !== undefined) {
-    return Math.random() * max;
+    return round(Math.random())
+  } else if (max == undefined && min !== undefined) {
+    return Math.random() * min;
   } else {
-    return Math.random() * (max - min + 1) + min;
+    return round(Math.random() * (max - min + 1) + min)
   }
 }
 
-function log(x) {
-  console.log(x);
+function log(...x) {
+  console.log(...x);
 }
 
-function error(a) {
-  console.error(a);
+function error(...a) {
+  console.error(...a);
 }
 
-function warn(s) {
-  console.warn(s);
+function warn(...s) {
+  console.warn(...s);
 }
 
 function kns(s) {
-  c = "";
-  for (as of s) {
+  var c = "";
+  for (var as of s) {
     c += as;
   }
   return c;
@@ -403,7 +379,7 @@ HTMLElement.prototype.css = function css(s, v = 0) {
   if (s && v) {
     this.style[s] = v;
   } else if (s.constructor.toString().indexOf("Object") > -1) {
-    for (i in s) {
+    for (var i in s) {
       this.style[i] = s[i];
     }
   } else if (s && v == 0) {
@@ -413,21 +389,21 @@ HTMLElement.prototype.css = function css(s, v = 0) {
     console.error(err("css", "1"));
   }
 };
-HTMLElement.prototype.add = function add(className) {
+HTMLElement.prototype.Add = function add(className) {
   if (this !== null && className !== undefined) {
     this.classList.add(className);
   } else {
     console.error(err("add", 2));
   }
 };
-HTMLElement.prototype.remov = function remove(className) {
+HTMLElement.prototype.Remove = function remove(className) {
   if (this !== null && className !== undefined) {
     this.classList.remove(className);
   } else {
     console.error(err("remove", 2));
   }
 };
-HTMLElement.prototype.toggle = function toggle(className) {
+HTMLElement.prototype.Toggle = function toggle(className) {
   if (this !== null && className !== undefined) {
     this.classList.toggle(className);
   } else {
@@ -436,20 +412,20 @@ HTMLElement.prototype.toggle = function toggle(className) {
 };
 HTMLElement.prototype.len = len;
 HTMLElement.prototype.attr = function attr(attr) {
-  for (a in attr) {
+  for (var a in attr) {
     this.setAttribute(a, attr[a]);
   }
 };
 HTMLElement.prototype.width = function () {
   return this.getBoundingClientRect().width;
 };
-HTMLElement.prototype.width = function () {
+HTMLElement.prototype.height = function () {
   return this.getBoundingClientRect().height;
 };
 HTMLElement.prototype.replace = function replace(tg) {
   this.innerHTML = this.textContent.replace(/[a-zA-z]/g, `<${tg}>$&</${tg}>`);
-  let bugs = document.querySelectorAll(tg);
-  for (bad of bugs) {
+  var bugs = document.querySelectorAll(tg);
+  for (var bad of bugs) {
     if (!bad.text()) bad.html("&nbsp");
   }
 };
@@ -516,8 +492,41 @@ HTMLElement.prototype.on = function (op, cx) {
   } catch (e) {}
 };
 
+
+class CreateApp {
+    constructor(app, el) {
+        this.app = app;
+        this.el = document.querySelector(el);
+        this.text = this.el.innerHTML;
+        this.template = '';
+   }
+
+    makeTemplate() {
+        var app = this.app;
+        var text = this.text;
+        var b = text.replace(/\{\{[ ]{0,}([a-zA-Z0-9.()]{1,})[ ]{0,}\}\}/gi, function (...match) {
+            var m = match[1];
+            try {
+                return eval('app.' + m);
+            } catch (err) {
+                throw new Error(err);
+            }
+        });
+        this.template = b;
+    }
+    render() {
+        this.makeTemplate();
+        this.el.innerHTML = this.template;
+    }
+   
+}
+
+function createApp(app, el) {
+    return new CreateApp(app, el);
+}
+
 Array.prototype.mix = function () {
-  let currentIndex = this.length,
+  var currentIndex = this.length,
     randomIndex;
   while (currentIndex != 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
@@ -533,18 +542,17 @@ HTMLInputElement.prototype.readFile = function readFile() {
   /*readOnly*/
   if (this instanceof HTMLInputElement && /file/i.test(this.type)) {
     try {
-      r = new FileReader();
-      f = this.files[0];
+      var r = new FileReader();
+      var f = this.files[0];
       r.readAsText(f);
-      r.onload = function () {
+      r.onload = async function () {
         console.log(r.result);
       };
       r.onerror = function () {
         console.log("Error");
       };
-      return r;
     } catch (e) {
-      warn("In first You have to choose at least one file");
+      warn("In first You have to choose at least one file\n"+  e);
     }
   } else if (!(this instanceof HTMLInputElement)) {
     console.warn("Element Must be input  element");
@@ -591,7 +599,17 @@ HTMLElement.prototype.frontdelete = function frontdelete() {
     );
   }
 };
-
+var a = `accentColor,additiveSymbols,alignContent,alignItems,alignSelf,alignmentBaseline,all,animation,animationDelay,animationDirection,animationDuration,animationFillMode,animationIterationCount,animationName,animationPlayState,animationTimingFunction,appRegion,appearance,ascentOverride,aspectRatio,backdropFilter,backfaceVisibility,background,backgroundAttachment,backgroundBlendMode,backgroundClip,backgroundColor,backgroundImage,backgroundOrigin,backgroundPosition,backgroundPositionX,backgroundPositionY,backgroundRepeat,backgroundRepeatX,backgroundRepeatY,backgroundSize,baselineShift,blockSize,border,borderBlock,borderBlockColor,borderBlockEnd,borderBlockEndColor,borderBlockEndStyle,borderBlockEndWidth,borderBlockStart,borderBlockStartColor,borderBlockStartStyle,borderBlockStartWidth,borderBlockStyle,borderBlockWidth,borderBottom,borderBottomColor,borderBottomLeftRadius,borderBottomRightRadius,borderBottomStyle,borderBottomWidth,borderCollapse,borderColor,borderEndEndRadius,borderEndStartRadius,borderImage,borderImageOutset,borderImageRepeat,borderImageSlice,borderImageSource,borderImageWidth,borderInline,borderInlineColor,borderInlineEnd,borderInlineEndColor,borderInlineEndStyle,borderInlineEndWidth,borderInlineStart,borderInlineStartColor,borderInlineStartStyle,borderInlineStartWidth,borderInlineStyle,borderInlineWidth,borderLeft,borderLeftColor,borderLeftStyle,borderLeftWidth,borderRadius,borderRight,borderRightColor,borderRightStyle,borderRightWidth,borderSpacing,borderStartEndRadius,borderStartStartRadius,borderStyle,borderTop,borderTopColor,borderTopLeftRadius,borderTopRightRadius,borderTopStyle,borderTopWidth,borderWidth,bottom,boxShadow,boxSizing,breakAfter,breakBefore,breakInside,bufferedRendering,captionSide,caretColor,clear,clip,clipPath,clipRule,color,colorInterpolation,colorInterpolationFilters,colorRendering,colorScheme,columnCount,columnFill,columnGap,columnRule,columnRuleColor,columnRuleStyle,columnRuleWidth,columnSpan,columnWidth,columns,contain,containIntrinsicBlockSize,containIntrinsicHeight,containIntrinsicInlineSize,containIntrinsicSize,containIntrinsicWidth,content,contentVisibility,counterIncrement,counterReset,counterSet,cursor,cx,cy,d,descentOverride,direction,display,dominantBaseline,emptyCells,fallback,fill,fillOpacity,fillRule,filter,flex,flexBasis,flexDirection,flexFlow,flexGrow,flexShrink,flexWrap,float,floodColor,floodOpacity,font,fontDisplay,fontFamily,fontFeatureSettings,fontKerning,fontOpticalSizing,fontSize,fontStretch,fontStyle,fontVariant,fontVariantCaps,fontVariantEastAsian,fontVariantLigatures,fontVariantNumeric,fontVariationSettings,fontWeight,forcedColorAdjust,gap,grid,gridArea,gridAutoColumns,gridAutoFlow,gridAutoRows,gridColumn,gridColumnEnd,gridColumnGap,gridColumnStart,gridGap,gridRow,gridRowEnd,gridRowGap,gridRowStart,gridTemplate,gridTemplateAreas,gridTemplateColumns,gridTemplateRows,height,hyphens,imageOrientation,imageRendering,inherits,initialValue,inlineSize,inset,insetBlock,insetBlockEnd,insetBlockStart,insetInline,insetInlineEnd,insetInlineStart,isolation,justifyContent,justifyItems,justifySelf,left,letterSpacing,lightingColor,lineBreak,lineGapOverride,lineHeight,listStyle,listStyleImage,listStylePosition,listStyleType,margin,marginBlock,marginBlockEnd,marginBlockStart,marginBottom,marginInline,marginInlineEnd,marginInlineStart,marginLeft,marginRight,marginTop,marker,markerEnd,markerMid,markerStart,mask,maskType,maxBlockSize,maxHeight,maxInlineSize,maxWidth,maxZoom,minBlockSize,minHeight,minInlineSize,minWidth,minZoom,mixBlendMode,negative,objectFit,objectPosition,offset,offsetDistance,offsetPath,offsetRotate,opacity,order,orientation,orphans,outline,outlineColor,outlineOffset,outlineStyle,outlineWidth,overflow,overflowAnchor,overflowClipMargin,overflowWrap,overflowX,overflowY,overscrollBehavior,overscrollBehaviorBlock,overscrollBehaviorInline,overscrollBehaviorX,overscrollBehaviorY,pad,padding,paddingBlock,paddingBlockEnd,paddingBlockStart,paddingBottom,paddingInline,paddingInlineEnd,paddingInlineStart,paddingLeft,paddingRight,paddingTop,page,pageBreakAfter,pageBreakBefore,pageBreakInside,pageOrientation,paintOrder,perspective,perspectiveOrigin,placeContent,placeItems,placeSelf,pointerEvents,position,prefix,quotes,r,range,resize,right,rowGap,rubyPosition,rx,ry,scrollBehavior,scrollMargin,scrollMarginBlock,scrollMarginBlockEnd,scrollMarginBlockStart,scrollMarginBottom,scrollMarginInline,scrollMarginInlineEnd,scrollMarginInlineStart,scrollMarginLeft,scrollMarginRight,scrollMarginTop,scrollPadding,scrollPaddingBlock,scrollPaddingBlockEnd,scrollPaddingBlockStart,scrollPaddingBottom,scrollPaddingInline,scrollPaddingInlineEnd,scrollPaddingInlineStart,scrollPaddingLeft,scrollPaddingRight,scrollPaddingTop,scrollSnapAlign,scrollSnapStop,scrollSnapType,scrollbarGutter,shapeImageThreshold,shapeMargin,shapeOutside,shapeRendering,size,sizeAdjust,speak,speakAs,src,stopColor,stopOpacity,stroke,strokeDasharray,strokeDashoffset,strokeLinecap,strokeLinejoin,strokeMiterlimit,strokeOpacity,strokeWidth,suffix,symbols,syntax,system,tabSize,tableLayout,textAlign,textAlignLast,textAnchor,textCombineUpright,textDecoration,textDecorationColor,textDecorationLine,textDecorationSkipInk,textDecorationStyle,textDecorationThickness,textIndent,textOrientation,textOverflow,textRendering,textShadow,textSizeAdjust,textTransform,textUnderlineOffset,textUnderlinePosition,top,touchAction,transform,transformBox,transformOrigin,transformStyle,transition,transitionDelay,transitionDuration,transitionProperty,transitionTimingFunction,unicodeBidi,unicodeRange,userSelect,userZoom,vectorEffect,verticalAlign,visibility,webkitAlignContent,webkitAlignItems,webkitAlignSelf,webkitAnimation,webkitAnimationDelay,webkitAnimationDirection,webkitAnimationDuration,webkitAnimationFillMode,webkitAnimationIterationCount,webkitAnimationName,webkitAnimationPlayState,webkitAnimationTimingFunction,webkitAppRegion,webkitAppearance,webkitBackfaceVisibility,webkitBackgroundClip,webkitBackgroundOrigin,webkitBackgroundSize,webkitBorderAfter,webkitBorderAfterColor,webkitBorderAfterStyle,webkitBorderAfterWidth,webkitBorderBefore,webkitBorderBeforeColor,webkitBorderBeforeStyle,webkitBorderBeforeWidth,webkitBorderBottomLeftRadius,webkitBorderBottomRightRadius,webkitBorderEnd,webkitBorderEndColor,webkitBorderEndStyle,webkitBorderEndWidth,webkitBorderHorizontalSpacing,webkitBorderImage,webkitBorderRadius,webkitBorderStart,webkitBorderStartColor,webkitBorderStartStyle,webkitBorderStartWidth,webkitBorderTopLeftRadius,webkitBorderTopRightRadius,webkitBorderVerticalSpacing,webkitBoxAlign,webkitBoxDecorationBreak,webkitBoxDirection,webkitBoxFlex,webkitBoxOrdinalGroup,webkitBoxOrient,webkitBoxPack,webkitBoxReflect,webkitBoxShadow,webkitBoxSizing,webkitClipPath,webkitColumnBreakAfter,webkitColumnBreakBefore,webkitColumnBreakInside,webkitColumnCount,webkitColumnGap,webkitColumnRule,webkitColumnRuleColor,webkitColumnRuleStyle,webkitColumnRuleWidth,webkitColumnSpan,webkitColumnWidth,webkitColumns,webkitFilter,webkitFlex,webkitFlexBasis,webkitFlexDirection,webkitFlexFlow,webkitFlexGrow,webkitFlexShrink,webkitFlexWrap,webkitFontFeatureSettings,webkitFontSmoothing,webkitHighlight,webkitHyphenateCharacter,webkitJustifyContent,webkitLineBreak,webkitLineClamp,webkitLocale,webkitLogicalHeight,webkitLogicalWidth,webkitMarginAfter,webkitMarginBefore,webkitMarginEnd,webkitMarginStart,webkitMask,webkitMaskBoxImage,webkitMaskBoxImageOutset,webkitMaskBoxImageRepeat,webkitMaskBoxImageSlice,webkitMaskBoxImageSource,webkitMaskBoxImageWidth,webkitMaskClip,webkitMaskComposite,webkitMaskImage,webkitMaskOrigin,webkitMaskPosition,webkitMaskPositionX,webkitMaskPositionY,webkitMaskRepeat,webkitMaskRepeatX,webkitMaskRepeatY,webkitMaskSize,webkitMaxLogicalHeight,webkitMaxLogicalWidth,webkitMinLogicalHeight,webkitMinLogicalWidth,webkitOpacity,webkitOrder,webkitPaddingAfter,webkitPaddingBefore,webkitPaddingEnd,webkitPaddingStart,webkitPerspective,webkitPerspectiveOrigin,webkitPerspectiveOriginX,webkitPerspectiveOriginY,webkitPrintColorAdjust,webkitRtlOrdering,webkitRubyPosition,webkitShapeImageThreshold,webkitShapeMargin,webkitShapeOutside,webkitTapHighlightColor,webkitTextCombine,webkitTextDecorationsInEffect,webkitTextEmphasis,webkitTextEmphasisColor,webkitTextEmphasisPosition,webkitTextEmphasisStyle,webkitTextFillColor,webkitTextOrientation,webkitTextSecurity,webkitTextSizeAdjust,webkitTextStroke,webkitTextStrokeColor,webkitTextStrokeWidth,webkitTransform,webkitTransformOrigin,webkitTransformOriginX,webkitTransformOriginY,webkitTransformOriginZ,webkitTransformStyle,webkitTransition,webkitTransitionDelay,webkitTransitionDuration,webkitTransitionProperty,webkitTransitionTimingFunction,webkitUserDrag,webkitUserModify,webkitUserSelect,webkitWritingMode,whiteSpace,widows,width,willChange,wordBreak,wordSpacing,wordWrap,writingMode,x,y,zIndex`;
+var properties = a.split(",");
+for (var c = 0; c < properties.length; c++) {
+    if (document.querySelectorAll(`[${properties[c]}]`)) {
+    var el = document.querySelectorAll(`[${properties[c]}]`);
+    for (var all of el) {
+       all.style[properties[c]] = all.getAttribute(properties[c]);
+    }
+  }
+}
+a=""
 Window.prototype.on = function event(op, cx) {
   if (is.fnc(op.click)) {
     this.onclick = op.click;
@@ -648,14 +666,16 @@ Window.prototype.on = function event(op, cx) {
 };
 Array.prototype.len = len;
 String.prototype.mix = function mix(n) {
-  c = "";
-  for (as of this) {
+  var c = "";
+  for (var as of this) {
     c += as;
   }
-  chars = c;
+  var chars = c;
   if (n) {
     var passWordLength = n;
-  } else passWordLength = c.length;
+  } else {
+      passWordLength = c.length;
+  }
   var b = "";
   for (var i = 0; i < passWordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
@@ -671,42 +691,34 @@ String.prototype.mix = function mix(n) {
     return b;
   }
 };
+
+(function() {
+    var p = document.createElement('div');
+    var span = document.createElement('span');
+    p.className = "popup"
+    p.appendChild(span);
+    document.body.appendChild(p);
+    var pops = document.querySelectorAll('[popup]');
+    pops.forEach(el => {
+        el.style.cursor = "pointer"
+        el.on('mouseenter', function () {
+            p.style.display = "block";
+           p.style.opacity = 1 ;
+            p.style.transform = "translateY(0)" ;p.querySelector('span').text(this.getAttribute("popup"))
+          p.style.left = this.offsetLeft - ( p.getBoundingClientRect().width - this.getBoundingClientRect().width) / 2 + 'px';
+            p.style.top = this.offsetTop  + this.getBoundingClientRect().height + 10 + 'px';
+        })
+        el.on('mouseleave', function () {
+            p.style.opacity = 0
+            p.style.transform = "translateY(20px)";
+            
+        })
+    })
+})()
+
+
+
 String.prototype.reverse = function () {
   return kns(this).split("").reverse().join("");
 };
-
-class CreateApp {
-  constructor(app, el) {
-    this.app = app;
-    this.el = document.querySelector(el);
-    this.text = this.el.innerHTML;
-    this.template = "";
-  }
-
-  makeTemplate() {
-    let app = this.app;
-    let text = this.text;
-    let b = text.replace(
-      /\{\{[ ]{0,}([a-zA-Z0-9.()]{1,})[ ]{0,}\}\}/gi,
-      function (...match) {
-        let m = match[1];
-        try {
-          return eval("app." + m);
-        } catch (err) {
-          throw new Error(err);
-        }
-      }
-    );
-    this.template = b;
-  }
-  render() {
-    this.makeTemplate();
-    this.el.innerHTML = this.template;
-  }
-}
-
-function createApp(app, el) {
-  return new CreateApp(app, el);
-}
-
 console.log("No bugs found, everything is good ");
